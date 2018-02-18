9751387842bbdca09df6c99790d4bd37ed6b3cf8
Deoren Moor <deoren@users.noreply.github.com>
2018-02-18 12:30:30 -0600

    conf.py - Add general project info
    - copyright
    - project name
    - author
    - version
    - release

--- source/conf.py
+++ source/conf.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+# General information about the project.
+project = u'NGINX Unit'
+copyright = u'2017-2018, NGINX, Inc.'
+author = u'NGINX'
+
+version = '0.6'
+
+release = "{} beta".format(version)
+
 highlight_language = 'json'
 
 html_theme = 'theme'
