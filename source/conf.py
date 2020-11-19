f2e96708907172026bb2769f8c03a51447ee907a
Artem Konev <artem.konev@nginx.com>
2020-11-19 19:44:10 +0000

    Added info about the Unit 1.21.0 release.

--- source/conf.py
+++ source/conf.py
@@ -5,8 +5,8 @@ import os, sys
 project = 'NGINX Unit'
 author = 'NGINX, Inc.'
 copyright = '2017-2020'
-version = '1.20.0'
-release_date = 'October 8, 2020'
+version = '1.21.0'
+release_date = 'November 19, 2020'
 release = version
 
 highlight_language = 'json'
